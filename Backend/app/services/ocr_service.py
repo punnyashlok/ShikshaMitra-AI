506113ec6555928663175a7289f0505cba4e8ba0
Punnyashlok <shloktoya2007@gmail.com>
2026-07-13 20:21:07 +0530

    Lazy load EasyOCR

--- Backend/app/services/ocr_service.py
+++ Backend/app/services/ocr_service.py
@@ -1,9 +1,19 @@
 import easyocr
 
-reader = easyocr.Reader(["en"], gpu=False)
+reader = None
+
+def get_reader():
+    global reader
+
+    if reader is None:
+        reader = easyocr.Reader(["en"], gpu=False)
+
+    return reader
 
 
 def extract_text(image_path: str):
+    reader = get_reader()
+
     result = reader.readtext(image_path)
 
     text = " ".join([item[1] for item in result])
