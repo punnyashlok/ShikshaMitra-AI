2ce61d630be3d634dbeadca208818d2591101d8b
Punnyashlok <shloktoya2007@gmail.com>
2026-07-14 11:03:37 +0530

    Disable scanner import

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -5,7 +5,7 @@ from fastapi.middleware.cors import CORSMiddleware
 # Import API Routes
 # ---------------------------------
 from app.routes.chat import router as chat_router
-from app.routes.scanner import router as scanner_router
+#from app.routes.scanner import router as scanner_router
 from app.routes.quiz import router as quiz_router
 from app.routes.pdf import router as pdf_router
 from app.routes.pdf_chat import router as pdf_chat_router
